83c7f38d52666c0e543c191a8fc41f0d96ac2880
Pratham-ghadge <prathamtech24@gmail.com>
2025-10-28 12:26:55 +0530

    28-10-2025

--- Semaphore/ProducerConsumer.java
+++ Semaphore/ProducerConsumer.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
-class Semaphore
-{
+class Semaphore {
     public int value;
 
     public Semaphore(int i) // To initialize the semaphore object with initial value.
@@ -9,31 +8,27 @@ class Semaphore
         this.value = i;
     }
 
-    public boolean Wait()   // It checks the access or status of the buffer.
+    public boolean Wait() // It checks the access or status of the buffer.
     {
-        if(this.value > 0)
-        {
+        if (this.value > 0) {
             this.value--;
             return true;
-        }
-        else
+        } else
             return false;
     }
 
-    public void Signal()    // It releases the resources.
+    public void Signal() // It releases the resources.
     {
         this.value++;
     }
 }
 
-public class ProducerConsumer
-{
-    public static void main(String args[])
-    {
-        final int buffer_size = 10;    // size of the buffer.
-        int count = 0;      // The number of elements in the buffer.
-        int in = 0;     //It moves to the next available slot.
-        int out = 0;    //It moves the slot to consume.
+public class ProducerConsumer {
+    public static void main(String args[]) {
+        final int buffer_size = 10; // size of the buffer.
+        int count = 0; // The number of elements in the buffer.
+        int in = 0; // It moves to the next available slot.
+        int out = 0; // It moves the slot to consume.
         int[] buffer = new int[buffer_size];
 
         // Creating the Semaphore variables.
@@ -41,8 +36,7 @@ public class ProducerConsumer
         Semaphore empty = new Semaphore(buffer_size);
         Semaphore full = new Semaphore(0);
 
-        while(true)
-        {
+        while (true) {
             System.out.println("\n<--------------MENU--------------->");
             System.out.println("1. Produce");
             System.out.println("2. Consume");
@@ -52,73 +46,65 @@ public class ProducerConsumer
             Scanner sc = new Scanner(System.in);
             int choice = sc.nextInt();
 
-            switch(choice)
-            {
+            switch (choice) {
                 // Produce
-                case 1:
-                    {
-                        if(!empty.Wait())
-                        {
-                            System.out.println("The Buffer is full.");
-                            break;
-                        }
-                        if(!mutex.Wait())
-                        {
-                            System.out.println("The resource is busy.");
-                            empty.Signal();
-                            break;
-                        }
-
-                        // Defining the Critical Section
-                        System.out.println("Enter product: ");
-                        int temp = sc.nextInt();
-                        buffer[in] = temp;
-                        in = (in + 1) % buffer_size;
-                        count++;
-                        System.out.println("The produced item is : " + temp);
-
-                        mutex.Signal();
-                        full.Signal();
+                case 1: {
+                    if (!empty.Wait()) {
+                        System.out.println("The Buffer is full.");
                         break;
-
                     }
-
-                // Consume
-                case 2:
-                    {
-                        if(!full.Wait())
-                        {
-                            System.out.println("The Buffer is empty.");
-                            break;
-                        }
-                        if(!mutex.Wait())
-                        {
-                            System.out.println("The resource is busy.");
-                            full.Signal();
-                            break;
-                        }
-
-                        // Critical Section
-                        int temp = buffer[out];
-                        out = (out + 1) % buffer_size;
-                        count--;
-                        System.out.println("The item consumed is : " + temp);
-
-                        mutex.Signal();
+                    if (!mutex.Wait()) {
+                        System.out.println("The resource is busy.");
                         empty.Signal();
                         break;
                     }
 
-                case 3:
-                    {
-                        System.out.println("Items in buffer: " + count + "/" + buffer_size);
-                        System.out.println("Mutex: " + mutex.value + ", Empty: " + empty.value + ", Full: " + full.value);
+                    // Defining the Critical Section
+                    System.out.println("Enter product: ");
+                    int temp = sc.nextInt();
+                    buffer[in] = temp;
+                    in = (in + 1) % buffer_size;
+                    count++;
+                    System.out.println("The produced item is : " + temp);
+
+                    mutex.Signal();
+                    full.Signal();
+                    break;
+
+                }
+
+                // Consume
+                case 2: {
+                    if (!full.Wait()) {
+                        System.out.println("The Buffer is empty.");
                         break;
                     }
-                
+                    if (!mutex.Wait()) {
+                        System.out.println("The resource is busy.");
+                        full.Signal();
+                        break;
+                    }
+
+                    // Critical Section
+                    int temp = buffer[out];
+                    out = (out + 1) % buffer_size;
+                    count--;
+                    System.out.println("The item consumed is : " + temp);
+
+                    mutex.Signal();
+                    empty.Signal();
+                    break;
+                }
+
+                case 3: {
+                    System.out.println("Items in buffer: " + count + "/" + buffer_size);
+                    System.out.println("Mutex: " + mutex.value + ", Empty: " + empty.value + ", Full: " + full.value);
+                    break;
+                }
+
                 case 4:
                     return;
             }
-        }    
+        }
     }
 }
